f47f1fda5fd9df7fb26fe01488c5c796ddf63a9a
geomio <geoff.millner22@gmail.com>
2021-02-25 11:04:49 -0800

    simplify code, have buttons doing click functions

--- js/scripts.js
+++ js/scripts.js
@@ -15,31 +15,31 @@ function divide(number1, number2) {
 }
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("button#but1").click(function(event) {
    event.preventDefault();
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
     $("#output").text(result);
   });
-  $("form#sub").submit(function(event) {
+  $("button#but2").click(function(event) {
     event.preventDefault();
-     const number1 = parseInt($("#sub1").val());
-     const number2 = parseInt($("#sub2").val());
+     const number1 = parseInt($("#add1").val());
+     const number2 = parseInt($("#add2").val());
      const result = subtract(number1, number2);
      $("#output").text(result);
    });
-   $("form#mult").submit(function(event) {
+   $("button#but3").click(function(event) {
     event.preventDefault();
-     const number1 = parseInt($("#mult1").val());
-     const number2 = parseInt($("#mult2").val());
+     const number1 = parseInt($("#add1").val());
+     const number2 = parseInt($("#add2").val());
      const result = multiply(number1, number2);
      $("#output").text(result);
    });
-   $("form#divide").submit(function(event) {
+   $("button#but4").click(function(event) {
     event.preventDefault();
-     const number1 = parseInt($("#divide1").val());
-     const number2 = parseInt($("#divide2").val());
+     const number1 = parseInt($("#add1").val());
+     const number2 = parseInt($("#add1").val());
      const result = divide(number1, number2);
      $("#output").text(result);
    });
